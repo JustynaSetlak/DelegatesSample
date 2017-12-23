ce44fcc0653db9bf165c93f74f9102a892393226
JustynaSetlak <justa96.s1@gmail.com>
2017-12-23 16:06:02 +0100

    delegates sample

---
+++ DelegatesSample/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace DelegatesSample
+{
+    public class Calculator
+    {
+        public delegate int ArithmeticDelegate(int firstNumber, int seconNumber);
+
+        public double Divide(double firstNumber, double secondNumber, Func<int> error)
+        {
+            if (secondNumber == 0)
+            {
+                return error();
+            }
+            return firstNumber / secondNumber;
+        }
+
+        public int Substract(int firstNumber, int secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+
+        public int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public double GetRadical(double number)
+        {
+            if (number < 0)
+            {
+                number = GetModulus(number);
+            }
+
+            return Math.Sqrt(number);
+
+            double GetModulus(double negativeNumber)
+            {
+                return Math.Abs(negativeNumber);
+            }
+        }
+    }
+}
---
+++ DelegatesSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DelegatesSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+           
+            var divideResult = calculator.Divide(4, 0, () =>
+            {
+                Console.WriteLine("An error occured. Can't divide by 0"); return 0;
+            });
+            Console.WriteLine($" Divide: {divideResult}");
+
+            var operation = new Calculator.ArithmeticDelegate(calculator.Add);
+            var resultOfOperation = operation(1, 3);
+            Console.WriteLine($"Add: {resultOfOperation}");
+
+            var sqrt = calculator.GetRadical(-16);
+            Console.WriteLine($"Radical: {sqrt}");
+
+            Console.ReadKey();
+        }
+    }
+}
